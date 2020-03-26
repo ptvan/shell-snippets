19d26c0b7b927b75716eae5d8aabd4f19666da4d
ptvan <pvan@fredhutch.org>
2020-03-26 09:38:33 -0700

    small additions

--- bash_snippets.sh
+++ bash_snippets.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+# process a batch of files
 # this script works on the CMU Faces image collection
 # (https://archive.ics.uci.edu/ml/datasets/CMU+Face+Images)
 
@@ -17,9 +19,13 @@ else
     echo "ERROR : 'faces' subdir not found, exiting...."
 fi
 
+# simply rename a lot of files
+for f in *.png; do mv -n "$f" "${f/-0}"; done
+
 # run a command on all results of a `find`, quick and dirty:
 find ./ -type f -name "*.txt" -exec gedit "{}" \;
 
 # nicer version: print0 gives a NULL after each entry to handle tricky filenames
 # xargs batches args before passing down the pipe, a bit more efficient
-find . -type f -name "*.txt" -print0 | xargs -0 gedit
+find ./ -type f -name "*.txt" -print0 | xargs -0 gedit
+
