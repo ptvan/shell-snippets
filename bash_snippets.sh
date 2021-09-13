a15a3347ef6202881a10cb9d404a650269102038
Phu T. Van <nulzilla@gmail.com>
2021-09-13 13:57:41 -0700

    minor update

--- bash_snippets.sh
+++ bash_snippets.sh
@@ -119,6 +119,7 @@ echo "Here comes the output of my failing code" | tee >(pbcopy)
 
 ##### git/GitHub
 # clone from a PR
+git branch -D pullrequest
 git fetch origin pull/2/head
 git checkout -b pullrequest FETCH_HEAD
 
