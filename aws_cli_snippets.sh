184c8312717ade291fded517e53ee8809456344c
Phu T. Van <nulzilla@gmail.com>
2022-07-12 09:18:26 -0700

    updated AWS CLI snippets

--- aws_cli_snippets.sh
+++ aws_cli_snippets.sh
@@ -1,2 +1,5 @@
 # retrieve sequence index from FASTQ stored on S3
 aws s3 cp s3://path/to/my/fastq.gz - | gzip -d - |  head -4
+
+# list all instances names, public IP address and status
+aws ec2 describe-instances --query "Reservations[*].Instances[*].{PublicIP:PublicIpAddress,Name:Tags[?Key=='Name']|[0].Value,Status:State.Name}"
