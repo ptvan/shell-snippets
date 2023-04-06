1c028fed57eeece5b9e7c32fce5a890caea81ee4
ptvan <nulzilla@gmail.com>
2023-04-06 12:19:51 -0700

    updates

--- bash_snippets.sh
+++ bash_snippets.sh
@@ -118,8 +118,9 @@ shnsplit -f file.cue -t %n-%t -o flac file.flac
 # convert FLAC to MP3 using parallelized ffmpeg
 parallel ffmpeg -i {} -qscale:a 0 {.}.mp3 ::: ./*.flac
 
-# trim input video to a 6'50" segment starting at 9:10
-ffmpeg -ss 00:9:10 -i Video.mp4 -ss 00:1:00 -t 00:06:50 -c copy VideoClip.mp4
+# trim input video to a 6m50s long segment starting at time index 9:10
+# lead in 1 minute before this (index 8:10) to allow audio to sync
+ffmpeg -ss 00:8:10 -i Video.mp4 -ss 00:1:00 -t 00:06:50 -c copy VideoClip.mp4
 
 # trim a PDF to include only certain pages using qpdf
 qpdf original.pdf --pages . 2-18 -- trimmed.pdf
