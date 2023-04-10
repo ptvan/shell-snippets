e77452c1aef790fccef7526b6931c6318cf9380c
ptvan <nulzilla@gmail.com>
2023-04-10 06:17:41 -0700

    updated sed snippets

--- sed_snippets.sh
+++ sed_snippets.sh
@@ -4,58 +4,61 @@
 sed G
 
 # double-space a file that contains some double-spaced lines
-sed '/^$/d;G'
+sed '/^$/d;G' file.txt
 
 # insert a blank line above every line that matches `regex`
-sed '/regex/{x;p;x;}'
+sed '/regex/{x;p;x;}' file.txt
 
 # number each line of a file and left-align the number
-sed = filename | sed 'N;s/\n/\t/'
+sed = filename | sed 'N;s/\n/\t/' file.txt
 
 # number each non-empty line of a file
-sed '/./=' filename | sed '/./N; s/\n/ /'
+sed '/./=' filename | sed '/./N; s/\n/ /' file.txt
 
 # convert DOS/Windows newlines (CRLF) to UNIX newlines (LF)
-sed 's/.$//'
+sed 's/.$//' file.txt
 
 # alternatively:
-sed 's/^M$//'
+sed 's/^M$//' file.txt
 
 # convert UNIX newlines (LF) to DOS/Windows newlines (CRLF)
-sed "s/$/`echo -e \\\r`/"
+sed "s/$/`echo -e \\\r`/" file.txt
 
 # delete leading tabs and spaces from each line
-sed 's/^[ \t]*//'
+sed 's/^[ \t]*//' file.txt
 
 # delete trailing tabs and spaces from each line
-sed 's/[ \t]*$//'
+sed 's/[ \t]*$//' file.txt
 
 # substitute the fourth occurrence of "foo" with "bar"
-sed 's/foo/bar/4'
+sed 's/foo/bar/4' file.txt
 
 # substitute the *first* occurrence of "foo" with "bar"
-sed 's/\(.*\)foo\(.*foo\)/\1bar\2/'
+sed 's/\(.*\)foo\(.*foo\)/\1bar\2/' file.txt
 
 # substitute the *last* occurrence of "foo" with "bar"
-sed 's/\(.*\)foo/\1bar/'
+sed 's/\(.*\)foo/\1bar/' file.txt
 
 # substitute all occurrences of "foo" with "bar" on lines that also contains "baz"
-sed '/baz/s/foo/bar/g'
+sed '/baz/s/foo/bar/g' file.txt
 
 # substitute all occurrences of "foo" with "bar" on lines that DO NOT contains "baz"
-sed '/baz/!s/foo/bar/g'
+sed '/baz/!s/foo/bar/g' file.txt
 
 # change "scarlet", "ruby", or "puce" into "red"
-sed 's/scarlet/red/g;s/ruby/red/g;s/puce/red/g'
+sed 's/scarlet/red/g;s/ruby/red/g;s/puce/red/g' file.txt
 
 # reverse order of lines
-sed '1!G;h;$!d'
+sed '1!G;h;$!d' file.txt
 
 # print lines that are longer than 65 characters
-sed -n '/^.\{65\}/p' 
+sed -n '/^.\{65\}/p' file.txt 
 
 # delete all lines between "pattern1" and "pattern2"
 seq ‘/pattern1/,/pattern2/d’ < infile.txt > outfile.txt
 
 # replace "^A" with tab character
 sed -e "s/$(echo -e \\001)/\\echo -e '\t'/g" file.txt
+
+# print out the file _except_ lines 20-35
+sed '20,35d' file.txt
