4c9fbcf76fd769e067bf90a71bd1fa275c5d54b8
ptvan <nulzilla@gmail.com>
2025-08-21 10:31:16 -0700

    added gcloud storage rsync

--- gcloud_cli_snippets.sh
+++ gcloud_cli_snippets.sh
@@ -1,10 +1,12 @@
-# config
+## config
 gcloud config set project my-project-name-here
 
-# connect
+## connect
 gcloud compute ssh my-host-name-here --zone=us-central1-a
 
-# file operations
+## file operations
 gcloud storage cp gs://your-bucket/your/single/file  /home/local_user
 gcloud storage cp -r /home/local_user gs://your-bucket/path/to/remote/bucket 
 
+# note `rsync`, NOT `sync` like AWS
+gcloud storage rsync ./local_dir gs://remote-bucket/path/to/remote/dir --recursive --dry-run
