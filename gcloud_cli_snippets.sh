4793f6b5ca72bf9b97fc7a9c3cae0aea188c93c7
ptvan <nulzilla@gmail.com>
2025-08-26 07:07:55 -0700

    added gcloud hash

--- gcloud_cli_snippets.sh
+++ gcloud_cli_snippets.sh
@@ -10,3 +10,6 @@ gcloud storage cp -r /home/local_user gs://your-bucket/path/to/remote/bucket
 
 # note `rsync`, NOT `sync` like AWS
 gcloud storage rsync ./local_dir gs://remote-bucket/path/to/remote/dir --recursive --dry-run
+
+# MD5 files without downloading
+gsutil hash -m -h gs://your-bucket/path/to/your/file | grep 'md5' | awk '{print $3}'
