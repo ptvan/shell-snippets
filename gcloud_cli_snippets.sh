6044259683ec45714134a7bf2614e07e52025b21
ptvan <nulzilla@gmail.com>
2025-09-08 09:49:53 -0700

    minor addition

--- gcloud_cli_snippets.sh
+++ gcloud_cli_snippets.sh
@@ -5,6 +5,7 @@ gcloud config set project my-project-name-here
 gcloud compute ssh my-host-name-here --zone=us-central1-a
 
 ## file operations
+gcloud storage ls --long gs://your-bucket/
 gcloud storage cp gs://your-bucket/your/single/file  /home/local_user
 gcloud storage cp -r /home/local_user gs://your-bucket/path/to/remote/bucket 
 
